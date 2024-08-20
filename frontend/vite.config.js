a60e6fdc29a46eaef05199f319ce20f619d03e59
Abhinay Yadav <abhinayy48@gmail.com.com>
2024-08-21 00:02:02 +0530

    Deployment-changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../backend/dist',
-    emptyOutDir: true, // also necessary
-  },
   server: {
 		port: 3000,
 		// Get rid of the CORS error
