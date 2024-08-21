afae3a9dc684b273442ee6d4bf8bc8c47e587140
Abhinay Yadav <abhinayy48@gmail.com.com>
2024-08-21 12:44:00 +0530

    cors changes

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,13 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import cors from 'cors';
+import cors from "cors";
 import connectDB from "./config/dbConnect.js";
 import postRoute from "./routes/post.route.js";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
-
 
 morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(
@@ -20,30 +19,34 @@ app.use(
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-  origin: '*', // or '*' to allow all origins
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
-app.use("/api/posts", postRoute);  
- 
+app.use("/api/posts", postRoute);
+
 if (process.env.NODE_ENV === "production") {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	// react app
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  // react app
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 process.on("uncaughtException", function (err) {
   console.log(err);
   process.exit(1);
-}); 
- 
+});
+
 app.listen(port, () => {
   connectDB();
   console.log(`Server is running on port ${port}`);
 });
- 
